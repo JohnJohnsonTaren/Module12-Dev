fe1fe0775defdd74cb34324596e6b48a5abc56bf
JohnJohnsonTaren <tytar.den@gmail.com>
2025-08-13 20:35:38 +0300

    https://github.com/JohnJohnsonTaren/Module12-Dev.git

--- src/main/java/serviceCRUD/ClientService.java
+++ src/main/java/serviceCRUD/ClientService.java
@@ -6,8 +6,12 @@ import java.util.List;
 
 public interface ClientService {
     void create(Client client);
+
     Client read(Long id);
+
     void update(Client client);
+
     void delete(Client client);
+
     List<Client> listAll();
 }
--- src/main/java/serviceCRUD/PlanetService.java
+++ src/main/java/serviceCRUD/PlanetService.java
@@ -6,8 +6,12 @@ import java.util.List;
 
 public interface PlanetService {
     void create(Planet planet);
+
     Planet read(String id);
+
     void update(Planet planet);
+
     void delete(Planet planet);
+
     List<Planet> listAll();
 }
